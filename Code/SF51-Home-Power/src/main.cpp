fd8be2c1529ae0ec41f603ccfd62a77e46c0a245
ahmed <alomairi@mail.ru>
2023-04-26 01:38:01 +0300

    updated version

---
+++ Code/SF51-Home-Power/src/main.cpp
@@ -0,0 +1,211 @@
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <ArduinoOTA.h>
+#include <ArduinoJson.h>
+
+#include "settings.h" //GPIO NodeMCU good-pins
+#include "secret.h"   //Wifi and MQTT server info
+
+const char *ssid = ssidWifi;       // defined on secret.h
+const char *password = passWifi;   // defined on secret.h
+const char *mqtt_server = mqttURL; // defined on secret.h
+const char *deviceName = mqttClient ;
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+StaticJsonDocument<100> doc;
+StaticJsonDocument<300> updater;
+int device;
+int valuejson;
+String passjson;
+
+unsigned long currentMillis;
+int lastCount = 0;
+int count = 0;
+
+boolean PowerLock = true;
+unsigned long WifiDelayMillis = 0;
+const long WifiDelayInterval = 5000; // interval to check wifi and mqtt
+ 
+
+void setup_wifi()
+{
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  WiFi.hostname(deviceName); // DHCP Hostname (useful for finding device for static lease)
+  if (!WiFi.config(local_IP, gateway, subnet, primaryDNS, secondaryDNS))
+  {
+    Serial.println("STA Failed to configure");
+  }
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(300);
+    Serial.print(".");
+  }
+
+  randomSeed(micros());
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  for (unsigned int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Test if parsing succeeds.
+  DeserializationError error = deserializeJson(doc, payload);
+  if (error)
+  {
+    Serial.print(F("deserializeJson() failed: "));
+    Serial.println(error.c_str());
+    return;
+  }
+  // Print the values to data types
+  device = doc["device"].as<unsigned int>();
+  valuejson = doc["value"].as<unsigned int>();
+  passjson = doc["password"].as<String>();
+  Serial.print("device: ");
+  Serial.print(device);
+  Serial.print(" valuejson: ");
+  Serial.println(valuejson);
+
+  switch (device)
+  {
+  case 1:
+    if (valuejson == 0)
+    {
+      PowerLock = true;
+      client.publish("PowerLock", "ON");
+    }
+    else if (valuejson == 1)
+    {
+      PowerLock = false;
+      client.publish("PowerLock", "OFF");
+    }
+    break;
+
+  case 5:
+    if (valuejson == 5 && passjson == passon && !PowerLock)
+    {
+      digitalWrite(pinOn, HIGH);
+      delay(100);
+      digitalWrite(pinOn, LOW);
+      client.publish("PowerON", "ON");
+    }
+    else if (valuejson == 9 && passjson == passoff && !PowerLock)
+    {
+      client.publish("PowerON", "OFF");
+    }
+    break;
+
+  case 9:
+    if (valuejson == 5 && passjson == passon && !PowerLock)
+    {
+      client.publish("PowerOFF", "ON");
+    }
+    else if (valuejson == 9 && passjson == passoff && !PowerLock)
+    {
+      digitalWrite(pinOff, HIGH);
+      delay(100);
+      digitalWrite(pinOff, LOW);
+      client.publish("PowerOFF", "OFF");
+    }
+    break;
+
+  default:
+    Serial.print("Err device in case-switch invalid.");
+    break;
+  }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  if (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection... ");
+    // Attempt to connect
+    uint32_t timestamp=millis()/1000;
+    char clientid[23];
+    snprintf(clientid,23, mqttClient"%02X",timestamp);
+    Serial.print("Client ID: ");
+    Serial.println(clientid);
+
+    if (client.connect(clientid, mqttName, mqttPASS, mqttWillTopic, 0, true, "offline"))
+    {
+      // Serial.println("Connected");
+      // Once connected, publish an announcement...
+      client.publish(mqttWillTopic, "online", true);
+      // ... and resubscribe
+      client.subscribe(subscribeTopic);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      count = count + 1;
+    }
+  }
+}
+
+void setup()
+{
+  pinMode(pini, OUTPUT);
+  pinMode(pinOn, OUTPUT);
+  pinMode(pinOff, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+  client.setServer(mqtt_server, mqttPORT);
+  client.setCallback(callback);
+
+
+  ArduinoOTA.setHostname(mqttClient);
+  ArduinoOTA.setPort(otaPort);
+  ArduinoOTA.setPassword(otaPass);
+  ArduinoOTA.begin();
+}
+
+void loop()
+{
+  ArduinoOTA.handle();
+  currentMillis = millis();
+
+  if (currentMillis - WifiDelayMillis >= WifiDelayInterval)
+  {
+    WifiDelayMillis = currentMillis;
+    if (!client.connected())
+    {
+      Serial.println("reconnecting ...");
+      reconnect();
+    }
+    else if (lastCount != count)
+    {
+      char buffer[200];
+      updater["Disconnected"] = count;
+      serializeJson(updater, buffer);
+      client.publish(mqttDisconnectTopic, buffer, true);
+      lastCount = count;
+    }
+  }
+  client.loop();
+}
